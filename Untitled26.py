77cb1ca00f08e86903b2428e8fa03d4f74edd966
AdamCabsite1928 <86656037+AdamCabsite1928@users.noreply.github.com>
2021-07-27 11:28:10 +0100

    Add files via upload

---
+++ Untitled26.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+mylist=[14, "hello", 967]
+mylist[2]
+
+#because we have only 2 indexes not 6
+
+
+# In[22]:
+
+
+import pandas
+import numpy
+#because we write numpy and pandas without Capital letters
+
+
+# In[23]:
+
+
+print("python errors")
+#because we write print without capital letters and python errors inside parentheses with commas
+
+
+# In[20]:
+
+
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True]
+#because true isn't a string in mydictionary
+
+
+# In[3]:
+
+
+i=14
+while i<78:   
+    print(i)
+    i+=1
+#because print and i belong to the loop while so we use the indentation
+
+
+# In[4]:
+
+
+it=iter([1,2,3])
+next(it)
+next(it)
+next(it)
+# because we have only three items so we only so the fourth iteration is extra
+
+
+# In[6]:
+
+
+int('15')+15 
+#we can not concatenate 'str' and 'int' objects
+
+
+# In[7]:
+
+
+int(10)
+# python can not be coverted in integer
+
+
+# In[9]:
+
+
+print('python')
+#we can't put python in all courts because it doesn't make sense
+
+
+# In[13]:
+
+
+x=19/2
+#we can not divide a number by zero
+
+
+# In[ ]:
+
+
+
+
